eb3879cb89ebaa21244a05283aef9848300ad3b2
Sarah Kennelly <sek40@uclive.ac.nz>
2020-05-11 12:27:56 +1200

    Added action bar to garden activity, no functionality just logs what needs to happen

--- app/src/main/java/com/example/pocketgardener/GardenActivity.kt
+++ app/src/main/java/com/example/pocketgardener/GardenActivity.kt
@@ -2,6 +2,8 @@ package com.example.pocketgardener
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,4 +17,51 @@ class GardenActivity : AppCompatActivity(){
         nameTextView.text = name
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.garden_action_bar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.shareButton -> {
+            share()
+            true
+        }
+        R.id.reminderButton -> {
+            setReminder()
+            true
+        }
+        R.id.takePhotoButton -> {
+            takePhoto()
+            true
+        }
+        R.id.uploadPhotoButton -> {
+            uploadPhoto()
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun share() {
+        Log.d("ActionBar", "Share Progress")
+    }
+
+    private fun setReminder() {
+        Log.d("ActionBar", "Set Reminder")
+    }
+
+    private fun takePhoto() {
+        Log.d("ActionBar", "Take Photo")
+    }
+
+    private fun uploadPhoto() {
+        Log.d("ActionBar", "Upload Photo")
+    }
+
+
+
 }
